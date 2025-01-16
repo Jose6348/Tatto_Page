389bc45120f8935846ce34f8081d7eff344312a8
Jose6348 <jfalasco88@gmail.com>
2025-01-16 01:22:55 -0300

    Config

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router' // Se você estiver usando vue-router
 
+
+
 // Importando os pacotes do Font Awesome
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
